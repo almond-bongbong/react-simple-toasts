e7fd6c1a10ec52508c901c85d8d4b7cdcada2dd9
cmlee <bal.dongdongdong@gmail.com>
2019-09-29 17:46:09 +0900

    bug fix

--- example/src/App.js
+++ example/src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render () {
     return (
       <div className="example">
-        <button onClick={() => toast('Hello toast!')}>Toast</button>
+        <button onClick={() => toast('Hello toast!', 100000)}>Toast</button>
         <button onClick={() => toast('This message is displayed for 1 second.', 1000)}>One-second toast</button>
         <button onClick={() => toast('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.')}>Long Message</button>
       </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,108 +1,53 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import styled from 'styled-components';
+import styles from './style.css';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { addRootElement, createElement } from './lib/generateElement';
 
-const toastContainer = document.getElementById('toast_container');
-if (!toastContainer) addRootElement(createElement('toast_container'));
+const toastContainer = document.getElementById(styles['toast_container']);
+if (!toastContainer) addRootElement(createElement(styles['toast_container']));
 const toastComponentList: any[] = [];
 
-const ToastContainer = styled.div`
-  box-sizing: border-box;
-  position: fixed;
-  bottom: 20px;
-  right: 30px;
-  z-index: 1000;
-  line-height: 1.5;
-  transform: translateZ(0);
-  
-  & * {
-    box-sizing: border-box;
-  }
-`;
-
-interface IToastMessage {
-  messageHeight: number,
-}
-
-const ToastMessage = styled.div`
-  margin-bottom: 10px;
-  text-align: right;
-  font-size: 0;
-  transition: height .3s, opacity .3s, transform .3s;
-
-  &.toast-enter {
-    opacity: 0;
-    height: 0;
-    transform: translateY(10px);
-  }
-  
-  &.toast-enter-active {
-    opacity: 1;
-    height: ${(props: IToastMessage) => props.messageHeight}px;
-    transform: translateY(0px);
-  }
-  
-  &.toast-leave {
-    opacity: 1;
-    transform: translateY(0px);
-  }
-  
-  &.toast-leave-active {
-    opacity: 0;
-    transform: translateY(-10px);
-  }
-
-  & .message {
-    display: inline-block;
-    overflow: hidden;
-    min-width: 280px;
-    max-width: 80%;
-    padding: 10px 20px;
-    background: rgba(0,0,10,0.4);
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
-    border-radius: 2px;
-    color: #fff;
-    font-size: 13px;
-    text-align: center;
-  }
-`;
-
 const renderDOM = () => {
-  const container = document.getElementById('toast_container');
+  const container = document.getElementById(styles['toast_container']);
   ReactDOM.render(
-    <ToastContainer>
-      <ReactCSSTransitionGroup
-        transitionName="toast"
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={300}
-      >
-        {toastComponentList.map(t => t.component)}
-      </ReactCSSTransitionGroup>
-    </ToastContainer>,
+    <div className={styles['toast-list']}>
+      <TransitionGroup classnames="list">
+        {toastComponentList.map(t => (
+          <CSSTransition key={t.id} timeout={5000} classNames="toast">
+            {t.component}
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
+    </div>,
     container,
   );
 };
 
 interface IToastProps {
-  message: string
+  message: string;
 }
 
 const Toast: React.FunctionComponent<IToastProps> = ({ message }) => {
   const messageDOM: any = useRef();
-  const [messageHeight, setMessageHeight] = useState<number>(100);
 
   useLayoutEffect(() => {
-    if (messageDOM.current) {
-      setMessageHeight(messageDOM.current.clientHeight);
+    if (messageDOM.current && messageDOM.current.clientHeight) {
+      const height = messageDOM.current.clientHeight;
+      messageDOM.current.style.height = '0px';
+      setTimeout(() => {
+        messageDOM.current.style.height = `${height}px`;
+      }, 0);
     }
   }, [messageDOM.current]);
 
   return (
-    <ToastMessage ref={messageDOM} className={messageHeight ? 'toast-enter' : ''} messageHeight={messageHeight}>
-      <div className="message">{message}</div>
-    </ToastMessage>
+    <div
+      ref={messageDOM}
+      className={`${styles['toast-message']}`}
+    >
+      <div className={styles['toast-content']}>{message}</div>
+    </div>
   );
 };
 
@@ -112,9 +57,7 @@ const toast = (message: string, time = 3000) => {
   const id = Date.now();
   toastComponentList.push({
     id,
-    component: (
-      <Toast key={id} message={message} />
-    ),
+    component: <Toast message={message} />,
   });
 
   renderDOM();
