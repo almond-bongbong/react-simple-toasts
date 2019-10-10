b195cb8524cef63e4f2e61891d82050c5a298f02
cmlee <bal.dongdongdong@gmail.com>
2019-10-10 20:29:01 +0900

    update default example

--- example/src/index.js
+++ example/src/index.js
@@ -2,11 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { toastConfig } from 'react-simple-toasts';
-
-toastConfig({
-  time: 5000,
-  className: 'my-toast-message'
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
