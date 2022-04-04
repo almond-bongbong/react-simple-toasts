d57ac96820ee9d4eaf36000c469d193d17233c5d
cmlee <bal.dongdongdong@gmail.com>
2022-04-04 22:50:07 +0900

    :arrow_up: update cra version

--- example/src/component/CommonHighlighter.js
+++ example/src/component/CommonHighlighter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import prism from 'react-syntax-highlighter/dist/cjs/styles/prism/prism';
-import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
+import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
+import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 
