3b25535be86aa8d724c9b07ff6ecf94f2e5dda86
cmlee <bal.dongdongdong@gmail.com>
2022-09-14 21:46:49 +0900

    :recycle: refactor code

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
